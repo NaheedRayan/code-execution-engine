ff3baf288f4382a889beb322b80b3487c8c8b842
NaheedRayan <naheed28ray@protonmail.com>
2021-06-01 13:25:35 +0600

    added logo

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const app = express()
 const port = 8080
 
 const cors = require('cors');
+const { fdatasync } = require('fs');
 app.use(cors())
 
 // middleware
@@ -31,6 +32,10 @@ function random(size) {
     return require("crypto").randomBytes(size).toString('hex');
 }
 
+// for printing json beautifully in response 
+// app.locals.pretty = true;
+app.set('json spaces', 2);
+
 app.get('/', (req, res) => {
     var os = require('os-utils');
 
@@ -43,15 +48,48 @@ app.get('/', (req, res) => {
         os.cpuFree(function (v) {
             cpu_free = v * 100;
             data = {
-                "OS platform": os.platform(),
-                "CPU usage (%)": cpu_usage,
-                "CPU free  (%)": cpu_free,
-                "CPU count": os.cpuCount() ,
-                "Free memory (mb)": os.freemem(),
-                "Total memory (mb)": os.totalmem(),
-                "Free memory (%)": os.freememPercentage()*100,
-                "OS Uptime (hour)": os.sysUptime()/3600,
-                "Avg Load (15min)": os.loadavg(15)*100
+                "logo":{
+                    "10":`  ............................................................................  `, 
+                    "11":`  .%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%.  `,  
+                    "12":`   ..%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%..   `,
+                    "13":`     ..................................  ..................................     `,
+                    "14":`       .%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%.  .%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%.       `,
+                    "15":`          .%%%%%%%%%%%%%%%%%%%%%%%%%%%.  .%%%%%%%%%%%%%%%%%%%%%%%%%%%.          `, 
+                    "16":`            ,,,,,,,,,,,,,,,,,,,,%%%%%%.  .%%%%%%,,,,,,,,,,,,,,,,,,,,            `,       
+                    "17":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "18":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "19":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "20":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "21":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "22":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "23":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "24":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "25":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "26":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "27":`                               .%%%%%%.  .%%%%%%.                               `,          
+                    "28":`                               .%%%%%%.  .%%%%%%.                               `,           
+                    "29":`                                 %%%%%.  .%%%%%                                 `,            
+                    "30":`                                  .%%%.  .%%%.                                  `,            
+                    "31":`                                     %.  .%                                     `,            
+                    "32":`                                                                                `,
+                    "33":`                                                                                `,                 
+                    "34":`                    ████████╗██╗████████╗ █████╗ ███╗   ██╗                     `,
+                    "35":`                    ╚══██╔══╝██║╚══██╔══╝██╔══██╗████╗  ██║                     `,
+                    "36":`                       ██║   ██║   ██║   ███████║██╔██╗ ██║                     `,
+                    "37":`                       ██║   ██║   ██║   ██╔══██║██║╚██╗██║                     `,
+                    "38":`                       ██║   ██║   ██║   ██║  ██║██║ ╚████║                     `,
+                    "39":`                       ╚═╝   ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝                     `,
+                    "40":`                           Titan Code Execution Engine                          `
+                },
+                "OS platform"       : os.platform(),
+                "CPU usage (%)"     : cpu_usage,
+                "CPU free  (%)"     : cpu_free,
+                "CPU count"         : os.cpuCount() ,
+                "Free memory (mb)"  : os.freemem(),
+                "Total memory (mb)" : os.totalmem(),
+                "Free memory (%)"   : os.freememPercentage()*100,
+                "OS Uptime (hour)"  : os.sysUptime()/3600,
+                "Avg Load (15min)"  : os.loadavg(15)*100
             }
             console.log(data);
             res.send(data);
@@ -95,8 +133,7 @@ app.post('/submit', (req, res) => {
         }
         client.setex(data_src.filename.toString(), 300, JSON.stringify(result));
 
-        // var host = window.location.protocol + "//" + window.location.host;
-        // console.log(host)
+       
 
         // res.status(202).send('http://localhost:8080/results/' + data_src.filename);
         res.status(202).send(req.protocol + '://' + req.get('host')+ "/results/" + data_src.filename);
